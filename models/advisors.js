dd8a27f146c25e25c98abb7ce3bad4d3d4bccb32
Anshul Singh <mail.anshul.singh@gmail.com>
2021-04-14 22:43:19 +0530

    trail deploy

--- models/advisors.js
+++ models/advisors.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const AdvisorSchema = new Schema({
     name: {
         type : String,
-        required: true
+        required: true,
+        unique : true
     },
     image: {
         type : String,
